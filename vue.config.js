fd7a888ffcd652c9dfa352e79fd66bced595a047
Pratap2018 <pratapmridha@gmail.com>
2024-08-06 20:24:50 +0530

    fix path

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,5 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  publicPath: process.env.NODE_ENV === "production" ? "/DIF_Hack" : "/",
+  publicPath: process.env.NODE_ENV === "production" ? "/HackAlongGallery" : "/",
   transpileDependencies: true
 })
